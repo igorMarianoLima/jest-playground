eef749ac1b2a4dcc86e86e48859e6ede48760f88
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-04-29 15:18:23 -0300

    refactor: remove unnused imports and variables

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,10 +1,7 @@
 import { Box, Stack, Typography } from "@mui/material";
-import { useState } from "react";
-import { TShirtCard } from "~/components/t-shirt-card";
+import { colorLabels, sizeLabels, TShirtCard } from "~/components/t-shirt-card";
 import { useCartReducerHook } from "~/hooks/use-cart-reducer.hook";
 import type { ITShirt } from "~/types/products/t-shirt.type";
-
-type TShirtsCart = (ITShirt & { amount: number })[];
 
 export function Welcome() {
   const { cart, dispatch } = useCartReducerHook();
@@ -24,7 +21,8 @@ export function Welcome() {
         {Array.from(cart.items).map(([key, item]) => (
           <Box key={key}>
             <Typography>
-              {item.quantity}x {item.title} {item.color} {item.size}
+              {item.quantity}x {item.title} {colorLabels[item.color]}{" "}
+              {sizeLabels[item.size]}
             </Typography>
           </Box>
         ))}
