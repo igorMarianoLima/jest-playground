68c738c05406670daa90ee35f81a8c3d65b81986
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-04-29 17:17:30 -0300

    feat: create mocked products to use in tests

---
+++ app/hooks/use-cart-reducer/mocks/products.mock.ts
@@ -0,0 +1,13 @@
+import type { ITShirt } from "~/types/products/t-shirt.type";
+
+export const MOCKED_TSHIRT_RED_P: ITShirt = {
+  title: "Camiseta",
+  color: "red",
+  size: "p",
+};
+
+export const MOCKED_TSHIRT_BLUE_M: ITShirt = {
+  title: "Camisa",
+  color: "blue",
+  size: "m",
+};
