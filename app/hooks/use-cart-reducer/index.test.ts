8123829817247e99823fe0dedeeb0fc1a864f67c
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-04-29 17:18:32 -0300

    test: implement cases of items remotion from cart in 'use-cart-hook' tests

--- app/hooks/use-cart-reducer/index.test.ts
+++ app/hooks/use-cart-reducer/index.test.ts
@@ -1,5 +1,8 @@
-import type { ITShirt } from "~/types/products/t-shirt.type";
 import { reducer, type CartState } from "./index.hook";
+import {
+  MOCKED_TSHIRT_BLUE_M,
+  MOCKED_TSHIRT_RED_P,
+} from "./mocks/products.mock";
 
 const initialState: CartState = {
   items: new Map(),
@@ -10,11 +13,7 @@ describe("Given an cart", () => {
     it('Should add a "Camiseta P vermelha"', () => {
       const state = reducer(initialState, {
         action: "add",
-        payload: {
-          title: "Camiseta",
-          color: "red",
-          size: "p",
-        },
+        payload: MOCKED_TSHIRT_RED_P,
       });
 
       const items = Array.from(state.items);
@@ -22,27 +21,21 @@ describe("Given an cart", () => {
 
       expect(items.length).toBe(1);
       expect(item).toBeTruthy();
-      expect(item.title).toBe("Camiseta");
-      expect(item.color).toBe("red");
-      expect(item.size).toBe("p");
+      expect(item.title).toBe(MOCKED_TSHIRT_RED_P.title);
+      expect(item.color).toBe(MOCKED_TSHIRT_RED_P.color);
+      expect(item.size).toBe(MOCKED_TSHIRT_RED_P.size);
       expect(item.quantity).toBe(1);
     });
 
     describe("When add a new item identical an already added", () => {
       it('Should keep same items (merge) but increase the "quantity"', () => {
-        const product: ITShirt = {
-          title: "Camiseta",
-          color: "red",
-          size: "p",
-        };
-
         let state = initialState;
         const itemsToAdd = 4;
 
         for (let i = 0; i < itemsToAdd; i++) {
           state = reducer(state, {
             action: "add",
-            payload: product,
+            payload: MOCKED_TSHIRT_RED_P,
           });
         }
 
@@ -51,9 +44,9 @@ describe("Given an cart", () => {
 
         expect(items.length).toBe(1);
         expect(item).toBeTruthy();
-        expect(item.title).toBe("Camiseta");
-        expect(item.color).toBe("red");
-        expect(item.size).toBe("p");
+        expect(item.title).toBe(MOCKED_TSHIRT_RED_P.title);
+        expect(item.color).toBe(MOCKED_TSHIRT_RED_P.color);
+        expect(item.size).toBe(MOCKED_TSHIRT_RED_P.size);
         expect(item.quantity).toBe(itemsToAdd);
       });
     });
@@ -61,11 +54,7 @@ describe("Given an cart", () => {
     it('Should add a "Camisa M azul"', () => {
       const state = reducer(initialState, {
         action: "add",
-        payload: {
-          title: "Camisa",
-          color: "blue",
-          size: "m",
-        },
+        payload: MOCKED_TSHIRT_BLUE_M,
       });
 
       const items = Array.from(state.items);
@@ -73,10 +62,77 @@ describe("Given an cart", () => {
 
       expect(items.length).toBe(1);
       expect(item).toBeTruthy();
-      expect(item.title).toBe("Camisa");
-      expect(item.color).toBe("blue");
-      expect(item.size).toBe("m");
+      expect(item.title).toBe(MOCKED_TSHIRT_BLUE_M.title);
+      expect(item.color).toBe(MOCKED_TSHIRT_BLUE_M.color);
+      expect(item.size).toBe(MOCKED_TSHIRT_BLUE_M.size);
       expect(item.quantity).toBe(1);
+    });
+  });
+
+  describe("When remove a item from a cart", () => {
+    it("Should remove the target item and keep others if there are", () => {
+      let state = initialState;
+      const itemsToAdd = 4;
+
+      for (let i = 0; i < itemsToAdd; i++) {
+        state = reducer(state, {
+          action: "add",
+          payload: MOCKED_TSHIRT_RED_P,
+        });
+
+        state = reducer(state, {
+          action: "add",
+          payload: MOCKED_TSHIRT_BLUE_M,
+        });
+      }
+
+      expect(state.items.size).toBe(2);
+
+      state = reducer(state, {
+        action: "remove",
+        payload: MOCKED_TSHIRT_BLUE_M,
+      });
+
+      const items = Array.from(state.items);
+      const item = items[0][1];
+
+      expect(items.length).toBe(1);
+      expect(item).toBeTruthy();
+      expect(item.title).toBe(MOCKED_TSHIRT_RED_P.title);
+      expect(item.color).toBe(MOCKED_TSHIRT_RED_P.color);
+      expect(item.size).toBe(MOCKED_TSHIRT_RED_P.size);
+      expect(item.quantity).toBe(itemsToAdd);
+    });
+
+    it("Should set to 1 quantity if it's the only item in cart", () => {
+      const tshirt = MOCKED_TSHIRT_RED_P;
+
+      let initialState: CartState = {
+        items: new Map(),
+      };
+      const itemsToAdd = 4;
+
+      for (let i = 0; i < itemsToAdd; i++) {
+        initialState = reducer(initialState, {
+          action: "add",
+          payload: tshirt,
+        });
+      }
+
+      expect(initialState.items.size).toBe(1);
+      expect(Array.from(initialState.items)[0][1].quantity).toBe(itemsToAdd);
+
+      const state = reducer(initialState, {
+        action: "remove",
+        payload: tshirt,
+      });
+
+      const item = Array.from(state.items)[0][1];
+      expect(state.items.size).toBe(1);
+      expect(item.quantity).toBe(1);
+      expect(item.title).toBe(tshirt.title);
+      expect(item.color).toBe(tshirt.color);
+      expect(item.size).toBe(tshirt.size);
     });
   });
 });
